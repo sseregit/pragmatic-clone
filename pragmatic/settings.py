961195fca18afcc2cad7945b6d6e1e3408cf54fd
JangYeonSik <sseregit@naver.com>
2021-12-09 15:20:23 +0900

    django course 29 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -139,6 +139,10 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
