ff9c4ca35bf3a0f5c726b73c4c3a237acfe76d9f
JangYeonSik <sseregit@naver.com>
2021-12-10 17:04:06 +0900

    django course 38 commit

--- commentapp/models.py
+++ commentapp/models.py
@@ -6,8 +6,8 @@ from articleapp.models import Article
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True, related_name='commentapp')
-    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='commentapp')
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True, related_name='comment')
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comment')
 
     content = models.TextField(null=False)
 
