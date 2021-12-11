112f170df1e33d46105167a663972791316c702c
JangYeonSik <sseregit@naver.com>
2021-12-11 16:56:52 +0900

    django course 40 commit

--- projectapp/models.py
+++ projectapp/models.py
@@ -6,3 +6,6 @@ class Project(models.Model):
     description = models.CharField(max_length=200, null=False)
     image = models.ImageField(upload_to='project/', null=True)
     created_at = models.DateField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
