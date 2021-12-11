8995a7591e36a2fa0ee8187a1cee7c39a6ab2361
JangYeonSik <sseregit@naver.com>
2021-12-11 15:10:01 +0900

    django course 40 commit

---
+++ projectapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=200, null=False)
+    image = models.ImageField(upload_to='project/', null=True)
+    created_at = models.DateField(auto_now_add=True, null=True)
