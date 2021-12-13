aed688ad91967402395af39ee711df02d0331d0d
sseregit <39255703+sseregit@users.noreply.github.com>
2021-12-14 01:54:27 +0900

    django course 60 commit

---
+++ pragmatic/settings/deploy.py
@@ -0,0 +1,34 @@
+from .base import *
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('MARIADB_NAME'),
+        'USER': env('MARIADB_USER'),
+        'PASSWORD': env('MARIADB_PASSWORD'),
+        'HOST': env('MARIADB_HOST'),
+        'PORT': env('MARIADB_PORT'),
+    }
+}
