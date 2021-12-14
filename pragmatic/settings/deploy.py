222af3a93b23036e6dbfcb837390ef8470026d84
sseregit <39255703+sseregit@users.noreply.github.com>
2021-12-14 16:00:48 +0900

    docker secret

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -1,5 +1,13 @@
 from .base import *
 
+def read_secret(secret_name):
+
+    file = open(f'/run/secrets/{secret_name}')
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+
+    return secret
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -12,7 +20,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -27,7 +35,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
